c6f9bf46b9d1f70748506a883f88a5263c2dd7ed
EVGENE <evgene.fe@gmail.com>
2020-07-09 18:12:28 +0300

    v.1.0.2

--- parser.py
+++ parser.py
@@ -1,15 +1,17 @@
 import requests
 from bs4 import BeautifulSoup
 
-URL = 'https://people.onliner.by/'
+URL = 'https://www.tut.by/'
 HEADERS = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:59.0) Gecko/20100101 Firefox/59.0'}
 
 r = requests.get(URL, headers=HEADERS)
 html = BeautifulSoup(r.content, 'html.parser')
-items = html.findAll('div', class_='news-tidings__item news-tidings__item_1of3 news-tidings__item_condensed')
+items = html.findAll('div', class_='news-entry ni')
 
 title = []
+
 for i in items:
-    title.append(i.find('span', class_='news-helpers_hide_mobile-small').get_text(strip=True))
+    title.append(i.find('span', class_='entry-head _title').get_text(strip=True))
+
 strong = '\n'.join(title)
 print(strong)
